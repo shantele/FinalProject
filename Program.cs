f6e248b9956a689367751023c9d869843da51e37
Daria <lp900@yandex.ru>
2022-09-20 01:36:13 +0300

    Created a C# code for the algorithm

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача: Написать программу, которая из 
+// имеющегося массива строк формирует новый
+// массив из строк, длина которых меньше,
+// либо равна 3 символам. Первоначальный 
+// массив можно ввести с клавиатуры, либо 
+// задать на старте выполнения алгоритма. 
+
+// Примеры:
+// [“Hello”, “2”, “world”, “:-)”] → [“2”, “:-)”]
+// [“1234”, “1567”, “-2”, “computer science”] → [“-2”]
+// [“Russia”, “Denmark”, “Kazan”] → []
+
+string[] initialArray = { "Hello", "2", "world", ":-)" };
+Console.WriteLine(String.Join(", ", initialArray));
+
+string[] shortArray = new string[initialArray.Length];
+int shortSize = 0;
+foreach (string element in initialArray)
+{
+    if (element.Length <= 3)
+    {
+        shortArray[shortSize] = element;
+        shortSize++;
+    }
+}
+
+Console.WriteLine(String.Join(", ", shortArray, 0, shortSize));
